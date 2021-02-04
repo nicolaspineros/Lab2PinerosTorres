f99c8578690d3efdf845af5a5458ee9b1a7c8336
Nicolas <onpc1224@gmail.com>
2021-02-03 20:43:23 -0500

    Esqueleto de la aplicacion completado

--- Patterns/src/main/java/edu/eci/cvds/patterns/shapes/ShapeFactory.java
+++ Patterns/src/main/java/edu/eci/cvds/patterns/shapes/ShapeFactory.java
@@ -1,5 +1,26 @@
 package edu.eci.cvds.patterns.shapes;
 
+import edu.eci.cvds.patterns.shapes.concrete.*;
+
 public class ShapeFactory {
     
+    public static Shape create(RegularShapeType type) {
+        
+        switch(type){
+            case Triangle:
+                return new Triangle();
+                
+            case Hexagon:
+                return new Hexagon();
+                
+            case Pentagon:
+                return new Pentagon();
+                
+            case Quadrilateral:
+                return new Quadrilateral();
+             
+            default:
+                throw new IllegalStateException("Valor no esperado: "+ type);
+        }
+    }
 }
